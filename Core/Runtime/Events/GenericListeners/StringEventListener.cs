622cd9cd0ee66be790512ef178adb2cd7129cd86
Sammey <samuelpetrus@gmail.com>
2021-12-21 11:03:51 +0100

    example listener

---
+++ Core/Runtime/Events/GenericListeners/StringEventListener.cs
@@ -0,0 +1,24 @@
+using Netherlands3D.Events;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StringEventListener : MonoBehaviour
+{
+    [SerializeField]
+    private StringEvent onEvent;
+
+    [SerializeField]
+    private StringUnityEvent trigger;
+    void Start()
+    {
+        onEvent.started.AddListener(Invoke);
+    }
+
+	private void Invoke(string arg0)
+	{
+        trigger.Invoke(arg0);
+    }
+}
