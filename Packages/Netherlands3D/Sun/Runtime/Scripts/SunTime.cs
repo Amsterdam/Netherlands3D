88fde5e57308760880ba4bed0a5891143245f43e
Sammey <samuelpetrus@gmail.com>
2022-04-04 15:43:25 +0200

    sun with basic events

---
+++ Packages/Netherlands3D/Sun/Runtime/Scripts/SunTime.cs
@@ -0,0 +1,92 @@
+using System;
+using UnityEngine;
+
+namespace Netherlands3D.Sun
+{
+    [ExecuteInEditMode]
+    public class SunTime : MonoBehaviour
+    {
+        [SerializeField]
+        private float longitude;
+
+        [SerializeField]
+        private float latitude;
+
+        [SerializeField]
+        [Range(0, 24)]
+        private int hour;
+
+        [SerializeField]
+        [Range(0, 60)]
+        private int minutes;
+
+        private DateTime time;
+
+        [SerializeField]
+        private Light sunDirectionalLight;
+
+        [SerializeField]
+        private float timeSpeed = 1;
+
+        [SerializeField]
+        private int frameSteps = 1;
+        private int frameStep;
+
+        public void SetTime(DateTime time) {
+            this.time = time;
+            OnValidate();
+        }
+
+        public void SetTime(int hour, int minutes) {
+            this.hour = hour;
+            this.minutes = minutes;
+            OnValidate();
+        }
+
+        public void SetLocation(float longitude, float latitude){
+          this.longitude = longitude;
+          this.latitude = latitude;
+        }
+
+        public void SetUpdateSteps(int i) {
+            frameSteps = i;
+        }
+
+        public void SetTimeSpeed(float speed) {
+            timeSpeed = speed;
+        }
+
+        private void Start()
+        {
+            time = DateTime.Now;
+            hour = time.Hour;
+            minutes = time.Minute;
+        }
+
+        private void OnValidate()
+        {
+            time = DateTime.Now.Date + new TimeSpan(hour, minutes, 0);
+        }
+
+        private void Update()
+        {
+            time = time.AddSeconds(timeSpeed * Time.deltaTime);
+            if (frameStep==0) {
+                SetPosition();
+            }
+            frameStep = (frameStep + 1) % frameSteps;
+		}
+
+        void SetPosition()
+        {
+            Vector3 angles = new Vector3();
+            double alt;
+            double azi;
+            SunPosition.CalculateSunPosition(time, (double)latitude, (double)longitude, out azi, out alt);
+            angles.x = (float)alt * Mathf.Rad2Deg;
+            angles.y = (float)azi * Mathf.Rad2Deg;
+
+            sunDirectionalLight.transform.localRotation = Quaternion.Euler(angles);
+        }        
+    }
+}
