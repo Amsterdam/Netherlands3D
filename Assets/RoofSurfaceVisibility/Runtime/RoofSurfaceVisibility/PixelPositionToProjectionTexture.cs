977410a3cf4d31aeb1810a2b32cb7b7eb5d44ab1
Sammey <samuelpetrus@gmail.com>
2022-05-06 11:33:09 +0200

    renamed script to be more descriptive

--- Assets/RoofSurfaceVisibility/Runtime/RoofSurfaceVisibility/PixelPositionToProjectionTexture.cs
+++ Assets/RoofSurfaceVisibility/Runtime/RoofSurfaceVisibility/PixelPositionToProjectionTexture.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public class GetPixelCoordinate : MonoBehaviour
+public class PixelPositionToProjectionTexture : MonoBehaviour
 {
     [SerializeField]
     private RenderTexture renderTexture;
