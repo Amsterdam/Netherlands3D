3af66cfcd4b164dcbda60e40caa5bcc57e97e09c
Sam Baas <samuelpetrus@gmail.com>
2022-08-01 15:02:49 +0200

    Feature/invoke events from editor (#36)
    * removed legacy script
    
    * added editor scripts to invoke main event types directly from their scriptable object container

---
+++ Packages/Netherlands3D/Core/Runtime/Editor/Scripts/Events/ColorEventEditor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Netherlands3D.Events;
+
+namespace Netherlands3D.Events
+{
+    [CustomEditor(typeof(ColorEvent))]
+    public class ColorEventEditor : Editor
+    {
+        private Color testPayload = Color.white;
+
+        public override void OnInspectorGUI()
+        {
+            if (Application.isPlaying)
+            {
+                testPayload = EditorGUILayout.ColorField("Payload", testPayload);
+                if (GUILayout.Button("Invoke"))
+                {
+                    var eventContainer = (ColorEvent)target;
+                    eventContainer.started.Invoke(testPayload);
+                    Debug.Log($"Invoked {eventContainer.name}");
+                }
+            }
+        }
+    }
+}
