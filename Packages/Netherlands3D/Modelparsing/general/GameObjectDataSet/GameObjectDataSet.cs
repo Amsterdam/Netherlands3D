2f7e77f8b6bd7865795a76fcf95708aee1166002
mvangog <44254917+mvangog@users.noreply.github.com>
2022-09-06 14:22:24 +0200

    Feature/import obj (#46)
    * create package
    
    * change material and gameObjectNames so that only the part up-to the miscrostation-file is used
    
    * remove tailing underscore in material.displayname
    
    * reuse the vectorList and intlist to avoid unneccesary memory allocation
    
    * dont create strings when you are not going to use them
    
    * reuse the SubmeshRawData-class instead of instantiate new ones

---
+++ Packages/Netherlands3D/Modelparsing/general/GameObjectDataSet/GameObjectDataSet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System;
+namespace Netherlands3D.ModelParsing
+{
+    [Serializable]
+    public class GameObjectDataSet
+    {
+        public string name;
+        public List<MaterialData> materials = new List<MaterialData>();
+        public List<GameObjectData> gameObjects = new List<GameObjectData>();
+        public void Clear()
+        {
+            foreach (GameObjectData item in gameObjects)
+            {
+                item.Clear();
+            }
+            gameObjects.Clear();
+            foreach (MaterialData item in materials)
+            {
+                item.Clear();
+            }
+            materials.Clear();
+        }
+    }
+
+}
