87a820ef6528347296f5d7d91c4095c4ac9d23f0
Sam Baas <samuelpetrus@gmail.com>
2022-09-20 13:49:34 +0200

    Feature/simple fast profile image (#51)
    * render camera for profile image with camera override
    
    * seperate preview texture for renderline
    
    * slicing with line based profile (fill disabled for now)
    
    * working before optimisation
    
    * improved performance ( still a lot of GC can be removed )
    
    * clipping with rendertexture
    
    * projector for cutting line
    
    * added received and canceled events to EventContainer base
    
    * example with poly drawing tool added. max point count added to polytool (default 1000)
    
    * added seperate system that cuts meshes using a line as input (two vector3's)
    
    * do now allow taps (placing points) if we are hovering UI
    
    * add intersection list
    
    * added triangle points and todo
    
    * saving profile as png
    
    * own triangle crossing gatherer
    
    * return intersection lines, and show gizmos on clipping object to preview
    
    * optional gizmos ( can be heavy with large meshes )
    
    * added dxf export script (receiving lines from profile generator)
    
    * proper webgl check
    
    * proper output name from variable
    
    * added fullscreen template
    
    * check for line bounds (ignore lines in output that fall outside bounds)
    
    * proper walk over lines
    
    * tidied variables in inspector
    
    * output event
    
    * svg export option ( scaleable vector graphic )
    
    * added tilehandler to make profiles on amsterdam geometry in sample. added svg for simple vector output
    
    * added simple progress indicator and events for smearing out memory heavy stuff over multiple frames
    
    * moved interface script to namespace and folder
    
    * render depths
    
    * Moved profilerendering into samples folder
    
    * added profile package to package.json

---
+++ Packages/Netherlands3D/ProfileRendering/Runtime/Scripts/ProfileRendering/OutputSVGLinesFile.cs
@@ -0,0 +1,92 @@
+using Netherlands3D.Events;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Xml;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+public class OutputSVGLinesFile : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void DownloadFile(byte[] array, int byteLength, string fileName);
+
+    [Header("Input")]
+    [SerializeField] Vector3ListEvent onReceiveLines;
+
+    [Header("Output")]
+    [SerializeField] FloatEvent outputProgress;
+
+    [Header("Settings")]
+    [SerializeField] private string outputFileName = "Profile_Netherlands3D.svg";
+    [SerializeField] private int addLinesPerFrame = 1000;
+    [SerializeField] private float strokeWidth = 0.1f;
+
+    void Awake()
+    {
+        onReceiveLines.started.AddListener(OutputAsSVG);
+    }
+
+    private void OutputAsSVG(List<Vector3> lines)
+    {
+        StartCoroutine(CreatSVGDocument(lines));
+    }
+
+    private IEnumerator CreatSVGDocument(List<Vector3> lines)
+    {
+        StringBuilder svgStringBuilder = new StringBuilder();
+
+        var minX = lines.Min(vector => vector.x);
+        var maxX = lines.Max(vector => vector.x);
+
+        var minY = lines.Min(vector => vector.y);
+        var maxY = lines.Max(vector => vector.y);
+
+        var width = (maxX - minX);
+        var height = (maxY - minY);
+
+        svgStringBuilder.AppendLine($"<svg viewBox=\"0 0 {width} {height}\" xmlns=\"http://www.w3.org/2000/svg\">");
+        for (int i = 0; i < lines.Count; i += 2)
+        {
+            if ((i % addLinesPerFrame) == 0)
+            {
+                if (outputProgress && i > 0) outputProgress.Invoke((float)i / lines.Count);
+                yield return new WaitForEndOfFrame();
+            }
+
+            var lineStart = lines[i]; 
+            var lineEnd = lines[i+1];
+
+            lineStart.x = (lineStart.x-minX);
+            lineStart.y = (lineStart.y - minY);
+
+            lineEnd.x = (lineEnd.x - minX);
+            lineEnd.y = (lineEnd.y - minY);
+
+            svgStringBuilder.AppendLine($"<line x1=\"{lineStart.x}\" y1=\"{height-lineStart.y}\" x2=\"{lineEnd.x}\" y2=\"{height-lineEnd.y}\" stroke=\"black\" stroke-width=\"{strokeWidth}\" />");
+        }
+        svgStringBuilder.AppendLine(" </svg>");
+
+        if (outputProgress) outputProgress.Invoke(1.0f);
+        yield return new WaitForEndOfFrame();
+        SaveFile(svgStringBuilder);
+
+    }
+    public void SaveFile(StringBuilder stringBuilder)
+    {
+#if UNITY_EDITOR 
+        string path = EditorUtility.SaveFilePanel("Save profile SVG", "", outputFileName, "svg");
+        if (path.Length != 0)
+        {
+            Debug.Log($"Saving svg: {path}");
+            File.WriteAllText(path, stringBuilder.ToString());
+        }
+#elif !UNITY_EDITOR && UNITY_WEBGL
+        byte[] buffer = Encoding.ASCII.GetBytes(stringBuilder.ToString());
+        DownloadFile(buffer, buffer.Length, outputFileName);
+#endif
+    }
+}
---
+++ Packages/Netherlands3D/ProfileRendering/Runtime/Scripts/netDxf-master/netDxf/Entities/Vertex.cs
@@ -0,0 +1,172 @@
+#region netDxf library licensed under the MIT License
+// 
+//                       netDxf library
+// Copyright (c) 2019-2021 Daniel Carvajal (haplokuon@gmail.com)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// 
+#endregion
+
+using netDxf.Tables;
+
+namespace netDxf.Entities
+{
+    /// <summary>
+    /// Represents a DXF Vertex.
+    /// </summary>
+    /// <remarks>
+    /// Under the VERTEX data the DXF stores information about the vertexes of smoothed Polylines2D (non-smoothed Polylines2D are stored as LWPOLYLINE,
+    /// Polylines3D (smoothed and non-smoothed), and PolyfaceMeshes.<br />
+    /// For internal use only.
+    /// </remarks>
+    internal class Vertex :
+        DxfObject
+    {
+        #region private fields
+
+        private VertexTypeFlags flags;
+        private Vector3 position;
+        private short[] vertexIndexes;
+        private double startWidth;
+        private double endWidth;
+        private double bulge;
+        private AciColor color;
+        private Layer layer;
+        private Linetype linetype;
+        private string subclassMarker;
+
+        #endregion
+
+        #region constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <c>Vertex</c> class.
+        /// </summary>
+        public Vertex()
+            : base(DxfObjectCode.Vertex)
+        {
+            this.flags = VertexTypeFlags.Polyline2DVertex;
+            this.position = Vector3.Zero;
+            this.layer = null;
+            this.color = null;
+            this.linetype = null;
+            this.bulge = 0.0;
+            this.startWidth = 0.0;
+            this.endWidth = 0.0;
+            this.vertexIndexes = null;
+            this.subclassMarker = netDxf.SubclassMarker.Polyline2DVertex;
+        }
+
+        #endregion
+
+        #region public properties
+
+        /// <summary>
+        /// Gets or sets the polyline vertex <see cref="Vector3">location</see>.
+        /// </summary>
+        public Vector3 Position
+        {
+            get { return this.position; }
+            set { this.position = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the indexes, only applicable when the vertex represents a face of a polyface mesh.
+        /// </summary>
+        public short[] VertexIndexes
+        {
+            get { return this.vertexIndexes; }
+            set { this.vertexIndexes = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the light weight polyline start segment width.
+        /// </summary>
+        public double StartWidth
+        {
+            get { return this.startWidth; }
+            set { this.startWidth = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the light weight polyline end segment width.
+        /// </summary>
+        public double EndWidth
+        {
+            get { return this.endWidth; }
+            set { this.endWidth = value; }
+        }
+
+        /// <summary>
+        /// Gets or set the light weight polyline bulge.Accepted values range from 0 to 1.
+        /// </summary>
+        public double Bulge
+        {
+            get { return this.bulge; }
+            set { this.bulge = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the vertex type.
+        /// </summary>
+        public VertexTypeFlags Flags
+        {
+            get { return this.flags; }
+            set { this.flags = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the entity color.
+        /// </summary>
+        public AciColor Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the entity layer.
+        /// </summary>
+        public Layer Layer
+        {
+            get { return this.layer; }
+            set { this.layer = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the entity line type.
+        /// </summary>
+        public Linetype Linetype
+        {
+            get { return this.linetype; }
+            set { this.linetype = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the subclass marker
+        /// </summary>
+        public string SubclassMarker
+        {
+            get { return this.subclassMarker; }
+            set { this.subclassMarker = value; }
+        }
+
+        #endregion
+    }
+}
