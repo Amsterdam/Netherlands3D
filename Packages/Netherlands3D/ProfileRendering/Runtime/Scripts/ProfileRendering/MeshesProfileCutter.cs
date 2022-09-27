7b86114aa9f211d514b151f7cccde66f703d03b8
Sammey <samuelpetrus@gmail.com>
2022-09-27 13:42:31 +0200

    dxf profile export now 'flattens' the profile on the x and y axes, and uses first point as 0 coordinate

--- Packages/Netherlands3D/ProfileRendering/Runtime/Scripts/ProfileRendering/MeshesProfileCutter.cs
+++ Packages/Netherlands3D/ProfileRendering/Runtime/Scripts/ProfileRendering/MeshesProfileCutter.cs
@@ -4,206 +4,209 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeshesProfileCutter : MonoBehaviour
+namespace Netherlands3D.ProfileRendering
 {
-    [Header("Input")]
-    [SerializeField] private TriggerEvent onCutMeshes;
-    [SerializeField] private Vector3ListEvent onReceiveCuttingLine;
-    [Header("Output")]
-    [SerializeField] private FloatEvent outputProgress;
-    [SerializeField] private Vector3ListEvent outputProfilePolyline;
-
-    private List<Vector3> cuttingLine;
-
-    [Header("Settings")]
-    [SerializeField, Tooltip("The meshes on these layers will be cut, and included in the profile")] 
-    private LayerMask layerMask;
-    [SerializeField] private bool drawInEditorGizmos = true;
-    [SerializeField] private int maxTrianglesPerFrame = 1000;
-
-    private Plane cuttingPlane;
-    private Bounds lineBounds;
-    private Vector3 lineCenter;
-
-    private List<Vector3> triangleIntersectionLine = new List<Vector3>();
-    private List<Vector3> profileLines = new List<Vector3>();
-
-    void Awake()
+    public class MeshesProfileCutter : MonoBehaviour
     {
-        onReceiveCuttingLine.started.AddListener(SetLine);
-        onCutMeshes.started.AddListener(CutMeshes);
+        [Header("Input")]
+        [SerializeField] private TriggerEvent onCutMeshes;
+        [SerializeField] private Vector3ListEvent onReceiveCuttingLine;
+        [Header("Output")]
+        [SerializeField] private FloatEvent outputProgress;
+        [SerializeField] private Vector3ListEvent outputProfilePolyline;
 
-        triangleIntersectionLine.Capacity = 2;
-    }
+        private List<Vector3> cuttingLine;
 
-    private void SetLine(List<Vector3> cuttingLine)
-    {
-        this.cuttingLine = cuttingLine;
-        lineBounds = GetBoundsFromLine(cuttingLine);
+        [Header("Settings")]
+        [SerializeField, Tooltip("The meshes on these layers will be cut, and included in the profile")]
+        private LayerMask layerMask;
+        [SerializeField] private bool drawInEditorGizmos = true;
+        [SerializeField] private int maxTrianglesPerFrame = 1000;
 
-        var planeNormal = Vector3.Cross((this.cuttingLine[0] - this.cuttingLine[1]).normalized, Vector3.up);
-        cuttingPlane = new Plane(planeNormal, lineCenter);
-    }
+        private Plane cuttingPlane;
+        private Bounds lineBounds;
+        private Vector3 lineCenter;
 
-    private Bounds GetBoundsFromLine(List<Vector3> line)
-    {
-        lineCenter = Vector3.Lerp(line[0], line[1], 0.5f);
-        var width = Mathf.Abs(line[0].x - line[1].x);
-        var depth = Mathf.Abs(line[0].z - line[1].z);
+        private List<Vector3> triangleIntersectionLine = new List<Vector3>();
+        private List<Vector3> profileLines = new List<Vector3>();
 
-        return new Bounds(lineCenter, new Vector3(width, 300, depth));
-    }
-
-    private void CutMeshes()
-    {
-        if(cuttingLine == null || cuttingLine.Count < 2)
+        void Awake()
         {
-            Debug.Log("Cant cut meshes. Cutting line not set.", this.gameObject);
-            return;
+            onReceiveCuttingLine.started.AddListener(SetLine);
+            onCutMeshes.started.AddListener(CutMeshes);
+
+            triangleIntersectionLine.Capacity = 2;
         }
 
-        var allMeshFilters = FindObjectsOfType<MeshFilter>();
-        StartCoroutine(CutLoop(allMeshFilters));
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        if (!drawInEditorGizmos) return;
-
-        if (lineBounds != null)
+        private void SetLine(List<Vector3> cuttingLine)
         {
-            Gizmos.color = new Color(1, 1, 0, 0.5f);
-            Gizmos.DrawCube(lineBounds.center, lineBounds.size);
+            this.cuttingLine = cuttingLine;
+            lineBounds = GetBoundsFromLine(cuttingLine);
+
+            var planeNormal = Vector3.Cross((this.cuttingLine[0] - this.cuttingLine[1]).normalized, Vector3.up);
+            cuttingPlane = new Plane(planeNormal, lineCenter);
         }
 
-        Gizmos.color = Color.green;
-        if (profileLines.Count > 1)
+        private Bounds GetBoundsFromLine(List<Vector3> line)
         {
-            for (int i = 0; i < profileLines.Count; i++)
+            lineCenter = Vector3.Lerp(line[0], line[1], 0.5f);
+            var width = Mathf.Abs(line[0].x - line[1].x);
+            var depth = Mathf.Abs(line[0].z - line[1].z);
+
+            return new Bounds(lineCenter, new Vector3(width, 300, depth));
+        }
+
+        private void CutMeshes()
+        {
+            if (cuttingLine == null || cuttingLine.Count < 2)
             {
-                Gizmos.DrawSphere(profileLines[i], 0.5f);
+                Debug.Log("Cant cut meshes. Cutting line not set.", this.gameObject);
+                return;
             }
 
-            for (int i = 0; i < profileLines.Count; i += 2)
-            {
-                Gizmos.DrawLine(profileLines[i], profileLines[i + 1]);
-            }
+            var allMeshFilters = FindObjectsOfType<MeshFilter>();
+            StartCoroutine(CutLoop(allMeshFilters));
         }
-    }
 
-    private IEnumerator CutLoop(MeshFilter[] meshFilters)
-    {
-        profileLines.Clear();
-        List<Vector3> meshProfile = new List<Vector3>();
-
-        if (outputProgress) outputProgress.Invoke(0.001f);
-        yield return new WaitForEndOfFrame();
-
-        for (int i = 0; i < meshFilters.Length; i++)
+        private void OnDrawGizmosSelected()
         {
-            var meshFilter = meshFilters[i];
-            if(outputProgress && i>0) outputProgress.Invoke( (float)i/meshFilters.Length );
+            if (!drawInEditorGizmos) return;
 
-            if (meshFilter && IsInLayerMask(meshFilter.gameObject.layer))
+            if (lineBounds != null)
             {
-                var renderer = meshFilter.GetComponent<Renderer>();
-                if (renderer != null && renderer.bounds.Intersects(lineBounds))
+                Gizmos.color = new Color(1, 1, 0, 0.5f);
+                Gizmos.DrawCube(lineBounds.center, lineBounds.size);
+            }
+
+            Gizmos.color = Color.green;
+            if (profileLines.Count > 1)
+            {
+                for (int i = 0; i < profileLines.Count; i++)
                 {
-                    Debug.Log($"Generating profile for {meshFilter.gameObject.name}", meshFilter.gameObject);
-                    meshProfile.Clear();
-                    yield return GetMeshProfile(meshFilter, meshProfile);
+                    Gizmos.DrawSphere(profileLines[i], 0.5f);
+                }
 
-                    profileLines.AddRange(meshProfile);
+                for (int i = 0; i < profileLines.Count; i += 2)
+                {
+                    Gizmos.DrawLine(profileLines[i], profileLines[i + 1]);
                 }
             }
         }
 
-        if (outputProgress) outputProgress.Invoke(0.001f);
-        outputProfilePolyline.Invoke(profileLines);
-
-        yield return null;
-    }
-
-    private IEnumerator GetMeshProfile(MeshFilter meshFilter, List<Vector3> meshProfile)
-    {
-        var targetMesh = meshFilter.sharedMesh;
-
-        var vertices = targetMesh.vertices;
-        var triangles = targetMesh.triangles;
-        Matrix4x4 localToWorld = meshFilter.transform.localToWorldMatrix;
-
-        //For each triangle, add the intersection line to our list
-        for (int i = 0; i < triangles.Length; i+=3)
+        private IEnumerator CutLoop(MeshFilter[] meshFilters)
         {
-            //Render a frame sometimes
-            if ((i % maxTrianglesPerFrame) == 0) yield return new WaitForEndOfFrame();
+            profileLines.Clear();
+            List<Vector3> meshProfile = new List<Vector3>();
 
-            triangleIntersectionLine.Clear();
+            if (outputProgress) outputProgress.Invoke(0.001f);
+            yield return new WaitForEndOfFrame();
 
-            //Get all vertex world positions ( so we support transformed objects )  
-            var pointAWorld = localToWorld.MultiplyPoint3x4(vertices[triangles[i]]);
-            var pointBWorld = localToWorld.MultiplyPoint3x4(vertices[triangles[i + 1]]);
-            var pointCWorld = localToWorld.MultiplyPoint3x4(vertices[triangles[i + 2]]);
-
-            Vector3 intersection;
-            if(LineCrossingCuttingPlane(pointAWorld, pointBWorld, out intersection))
+            for (int i = 0; i < meshFilters.Length; i++)
             {
-                triangleIntersectionLine.Add(intersection);
-            }
-            if (LineCrossingCuttingPlane(pointBWorld, pointCWorld, out intersection))
-            {
-                triangleIntersectionLine.Add(intersection);
-                if(triangleIntersectionLine.Count == 2)
+                var meshFilter = meshFilters[i];
+                if (outputProgress && i > 0) outputProgress.Invoke((float)i / meshFilters.Length);
+
+                if (meshFilter && IsInLayerMask(meshFilter.gameObject.layer))
                 {
-                    if (GetBoundsFromLine(triangleIntersectionLine).Intersects(lineBounds))
+                    var renderer = meshFilter.GetComponent<Renderer>();
+                    if (renderer != null && renderer.bounds.Intersects(lineBounds))
                     {
-                        meshProfile.AddRange(triangleIntersectionLine);
+                        Debug.Log($"Generating profile for {meshFilter.gameObject.name}", meshFilter.gameObject);
+                        meshProfile.Clear();
+                        yield return GetMeshProfile(meshFilter, meshProfile);
+
+                        profileLines.AddRange(meshProfile);
                     }
-                    continue; //We have our line, next triangle!
-                } 
+                }
             }
-            if (LineCrossingCuttingPlane(pointCWorld, pointAWorld, out intersection))
+
+            if (outputProgress) outputProgress.Invoke(0.001f);
+            outputProfilePolyline.Invoke(profileLines);
+
+            yield return null;
+        }
+
+        private IEnumerator GetMeshProfile(MeshFilter meshFilter, List<Vector3> meshProfile)
+        {
+            var targetMesh = meshFilter.sharedMesh;
+
+            var vertices = targetMesh.vertices;
+            var triangles = targetMesh.triangles;
+            Matrix4x4 localToWorld = meshFilter.transform.localToWorldMatrix;
+
+            //For each triangle, add the intersection line to our list
+            for (int i = 0; i < triangles.Length; i += 3)
             {
-                triangleIntersectionLine.Add(intersection);
-                if (triangleIntersectionLine.Count == 2)
+                //Render a frame sometimes
+                if ((i % maxTrianglesPerFrame) == 0) yield return new WaitForEndOfFrame();
+
+                triangleIntersectionLine.Clear();
+
+                //Get all vertex world positions ( so we support transformed objects )  
+                var pointAWorld = localToWorld.MultiplyPoint3x4(vertices[triangles[i]]);
+                var pointBWorld = localToWorld.MultiplyPoint3x4(vertices[triangles[i + 1]]);
+                var pointCWorld = localToWorld.MultiplyPoint3x4(vertices[triangles[i + 2]]);
+
+                Vector3 intersection;
+                if (LineCrossingCuttingPlane(pointAWorld, pointBWorld, out intersection))
                 {
-                    if (GetBoundsFromLine(triangleIntersectionLine).Intersects(lineBounds))
+                    triangleIntersectionLine.Add(intersection);
+                }
+                if (LineCrossingCuttingPlane(pointBWorld, pointCWorld, out intersection))
+                {
+                    triangleIntersectionLine.Add(intersection);
+                    if (triangleIntersectionLine.Count == 2)
                     {
-                        meshProfile.AddRange(triangleIntersectionLine);
+                        if (GetBoundsFromLine(triangleIntersectionLine).Intersects(lineBounds))
+                        {
+                            meshProfile.AddRange(triangleIntersectionLine);
+                        }
+                        continue; //We have our line, next triangle!
                     }
-                    continue; //We have our line, next triangle!
+                }
+                if (LineCrossingCuttingPlane(pointCWorld, pointAWorld, out intersection))
+                {
+                    triangleIntersectionLine.Add(intersection);
+                    if (triangleIntersectionLine.Count == 2)
+                    {
+                        if (GetBoundsFromLine(triangleIntersectionLine).Intersects(lineBounds))
+                        {
+                            meshProfile.AddRange(triangleIntersectionLine);
+                        }
+                        continue; //We have our line, next triangle!
+                    }
                 }
             }
         }
-    }
 
-    private bool LineCrossingCuttingPlane(Vector3 pointAWorld, Vector3 pointBWorld, out Vector3 intersection)
-    {
-        if (cuttingPlane.GetSide(pointAWorld) != cuttingPlane.GetSide(pointBWorld))
+        private bool LineCrossingCuttingPlane(Vector3 pointAWorld, Vector3 pointBWorld, out Vector3 intersection)
         {
-            intersection = PointOnPlaneBetweenTwoPoints(cuttingPlane, pointAWorld, pointBWorld);
-            return true;
+            if (cuttingPlane.GetSide(pointAWorld) != cuttingPlane.GetSide(pointBWorld))
+            {
+                intersection = PointOnPlaneBetweenTwoPoints(cuttingPlane, pointAWorld, pointBWorld);
+                return true;
+            }
+            intersection = Vector3.zero;
+            return false;
         }
-        intersection = Vector3.zero;
-        return false;
-    }
 
-    private Vector3 PointOnPlaneBetweenTwoPoints(Plane plane, Vector3 a, Vector3 b)
-    {
-        var q = (b - a);
-        q.Normalize();
-        Vector3 planeEqation;
-        var pointOnPlane = plane.ClosestPointOnPlane(Vector3.zero);
-        var normal = plane.normal;
-        planeEqation = normal;
-        var offset = Vector3.Dot(pointOnPlane, normal);
-        var t = (offset - Vector3.Dot(a, planeEqation)) / Vector3.Dot(q, planeEqation);
+        private Vector3 PointOnPlaneBetweenTwoPoints(Plane plane, Vector3 a, Vector3 b)
+        {
+            var q = (b - a);
+            q.Normalize();
+            Vector3 planeEqation;
+            var pointOnPlane = plane.ClosestPointOnPlane(Vector3.zero);
+            var normal = plane.normal;
+            planeEqation = normal;
+            var offset = Vector3.Dot(pointOnPlane, normal);
+            var t = (offset - Vector3.Dot(a, planeEqation)) / Vector3.Dot(q, planeEqation);
 
-        return a + (q * t);
-    }
+            return a + (q * t);
+        }
 
-    public bool IsInLayerMask(int layer)
-    {
-        return layerMask == (layerMask | (1 << layer));
+        public bool IsInLayerMask(int layer)
+        {
+            return layerMask == (layerMask | (1 << layer));
+        }
     }
 }
--- Packages/Netherlands3D/ProfileRendering/Runtime/Scripts/ProfileRendering/OutputDXFLinesFile.cs
+++ Packages/Netherlands3D/ProfileRendering/Runtime/Scripts/ProfileRendering/OutputDXFLinesFile.cs
@@ -10,73 +10,88 @@ using System.Runtime.InteropServices;
 using UnityEditor;
 using UnityEngine;
 
-public class OutputDXFLinesFile : MonoBehaviour
+namespace Netherlands3D.ProfileRendering
 {
-    [DllImport("__Internal")]
-    private static extern void DownloadFile(byte[] array, int byteLength, string fileName);
-    [Header("Input")]
-    [SerializeField] Vector3ListEvent onReceiveLines;
-    [Header("Output")]
-    [SerializeField] FloatEvent outputProgress;
-
-    [Header("Settings")]
-    [SerializeField] private bool binary = false;
-    [SerializeField] private int addLinesPerFrame = 1000;
-    [SerializeField] private string outputFileName = "Profile_Netherlands3D.dxf";
-
-
-    void Awake()
+    public class OutputDXFLinesFile : MonoBehaviour
     {
-        onReceiveLines.started.AddListener(OutputAsDXF);
-    }
+        [DllImport("__Internal")]
+        private static extern void DownloadFile(byte[] array, int byteLength, string fileName);
+        [Header("Input")]
+        [SerializeField] Vector3ListEvent onReceiveLines;
+        [Header("Output")]
+        [SerializeField] FloatEvent outputProgress;
 
-    private void OutputAsDXF(List<UnityEngine.Vector3> lines)
-    {
-        StartCoroutine(CreateDXFDocument(lines));
-    }
+        [Header("Settings")]
+        [SerializeField] private bool binary = false;
+        [SerializeField] private int addLinesPerFrame = 1000;
+        [SerializeField] private string outputFileName = "Profile_Netherlands3D.dxf";
 
-    private IEnumerator CreateDXFDocument(List<UnityEngine.Vector3> lines)
-    {
-        DxfDocument dxfDocument = new DxfDocument();
-        dxfDocument.DrawingVariables.InsUnits = netDxf.Units.DrawingUnits.Meters;
 
-        for (int i = 0; i < lines.Count; i += 2)
+        void Awake()
         {
-            if ((i % addLinesPerFrame) == 0)
+            onReceiveLines.started.AddListener(OutputAsDXF);
+        }
+
+        private void OutputAsDXF(List<UnityEngine.Vector3> lines)
+        {
+            StartCoroutine(CreateDXFDocument(lines));
+        }
+
+        private IEnumerator CreateDXFDocument(List<UnityEngine.Vector3> lines)
+        {
+            DxfDocument dxfDocument = new DxfDocument();
+            dxfDocument.DrawingVariables.InsUnits = netDxf.Units.DrawingUnits.Meters;
+
+            var first = lines[0];
+            first.y = 0;
+            var last = lines[lines.Count - 1];
+            last.y = 0;
+
+            var coordinateSystem = new GameObject().transform;
+            UnityEngine.Vector3 directionRight = (last - first).normalized;
+            coordinateSystem.position = first;
+            coordinateSystem.right = directionRight;
+
+            for (int i = 0; i < lines.Count; i += 2)
             {
-                if (outputProgress && i > 0) outputProgress.Invoke((float)i / lines.Count);
-                yield return new WaitForEndOfFrame();
+                if ((i % addLinesPerFrame) == 0)
+                {
+                    if (outputProgress && i > 0) outputProgress.Invoke((float)i / lines.Count);
+                    yield return new WaitForEndOfFrame();
+                }
+
+                var rdStart = coordinateSystem.InverseTransformPoint(lines[i]);
+                var rdEnd = coordinateSystem.InverseTransformPoint(lines[i + 1]);
+
+                netDxf.Vector2 lineStart = new netDxf.Vector2(rdStart.x, rdStart.y);
+                netDxf.Vector2 lineEnd = new netDxf.Vector2(rdEnd.x, rdEnd.y);
+
+                Line entity = new Line(lineStart, lineEnd);
+                dxfDocument.AddEntity(entity);
             }
 
-            var rdStart = CoordConvert.UnitytoRD(lines[i]);
-            var rdEnd = CoordConvert.UnitytoRD(lines[i + 1]);
+            Destroy(coordinateSystem.gameObject);
 
-            netDxf.Vector3 lineStart = new netDxf.Vector3(rdStart.x, rdStart.y, rdStart.z);
-            netDxf.Vector3 lineEnd = new netDxf.Vector3(rdEnd.x, rdEnd.y, rdEnd.z);
-
-            Line entity = new Line(lineStart, lineEnd);
-            dxfDocument.AddEntity(entity);
+            if (outputProgress) outputProgress.Invoke(1.0f);
+            yield return new WaitForEndOfFrame();
+            SaveFile(dxfDocument);
         }
-        if (outputProgress) outputProgress.Invoke(1.0f);
-        yield return new WaitForEndOfFrame();
-        SaveFile(dxfDocument);
-    }
 
-    public void SaveFile(DxfDocument dxfDocument)
-    {
+        public void SaveFile(DxfDocument dxfDocument)
+        {
 #if UNITY_EDITOR
-        string path = EditorUtility.SaveFilePanel("Save profile DXF", "", outputFileName, "dxf");
-        if (path.Length != 0)
-        {
-            if(dxfDocument.Save(path, binary))
+            string path = EditorUtility.SaveFilePanel("Save profile DXF", "", outputFileName, "dxf");
+            if (path.Length != 0)
             {
-                Debug.Log($"Saved: {path}");
+                if (dxfDocument.Save(path, binary))
+                {
+                    Debug.Log($"Saved: {path}");
+                }
+                else
+                {
+                    Debug.Log($"Could not save: {path}");
+                }
             }
-            else
-            {
-                Debug.Log($"Could not save: {path}");
-            }
-        }
 #elif !UNITY_EDITOR && UNITY_WEBGL
             using (var stream = new MemoryStream())
             {
@@ -91,6 +106,6 @@ public class OutputDXFLinesFile : MonoBehaviour
                 }
             }
 #endif
+        }
     }
 }
-
