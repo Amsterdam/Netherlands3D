96a7a23b9445ce51072085c6ed2c4e1545c9080a
Sam Baas <samuelpetrus@gmail.com>
2022-09-27 13:43:39 +0200

    dxf profile export now 'flattens' the profile on the x and y axes, and uses first point as 0 coordinate (#55)

--- Packages/Netherlands3D/ProfileRendering/Runtime/Scripts/ProfileRendering/OutputSVGLinesFile.cs
+++ Packages/Netherlands3D/ProfileRendering/Runtime/Scripts/ProfileRendering/OutputSVGLinesFile.cs
@@ -9,84 +9,87 @@ using UnityEditor;
 using UnityEngine;
 using System.Linq;
 
-public class OutputSVGLinesFile : MonoBehaviour
+namespace Netherlands3D.ProfileRendering
 {
-    [DllImport("__Internal")]
-    private static extern void DownloadFile(byte[] array, int byteLength, string fileName);
-
-    [Header("Input")]
-    [SerializeField] Vector3ListEvent onReceiveLines;
-
-    [Header("Output")]
-    [SerializeField] FloatEvent outputProgress;
-
-    [Header("Settings")]
-    [SerializeField] private string outputFileName = "Profile_Netherlands3D.svg";
-    [SerializeField] private int addLinesPerFrame = 1000;
-    [SerializeField] private float strokeWidth = 0.1f;
-
-    void Awake()
+    public class OutputSVGLinesFile : MonoBehaviour
     {
-        onReceiveLines.started.AddListener(OutputAsSVG);
-    }
+        [DllImport("__Internal")]
+        private static extern void DownloadFile(byte[] array, int byteLength, string fileName);
 
-    private void OutputAsSVG(List<Vector3> lines)
-    {
-        StartCoroutine(CreatSVGDocument(lines));
-    }
+        [Header("Input")]
+        [SerializeField] Vector3ListEvent onReceiveLines;
 
-    private IEnumerator CreatSVGDocument(List<Vector3> lines)
-    {
-        StringBuilder svgStringBuilder = new StringBuilder();
+        [Header("Output")]
+        [SerializeField] FloatEvent outputProgress;
 
-        var minX = lines.Min(vector => vector.x);
-        var maxX = lines.Max(vector => vector.x);
+        [Header("Settings")]
+        [SerializeField] private string outputFileName = "Profile_Netherlands3D.svg";
+        [SerializeField] private int addLinesPerFrame = 1000;
+        [SerializeField] private float strokeWidth = 0.1f;
 
-        var minY = lines.Min(vector => vector.y);
-        var maxY = lines.Max(vector => vector.y);
-
-        var width = (maxX - minX);
-        var height = (maxY - minY);
-
-        svgStringBuilder.AppendLine($"<svg viewBox=\"0 0 {width} {height}\" xmlns=\"http://www.w3.org/2000/svg\">");
-        for (int i = 0; i < lines.Count; i += 2)
+        void Awake()
         {
-            if ((i % addLinesPerFrame) == 0)
+            onReceiveLines.started.AddListener(OutputAsSVG);
+        }
+
+        private void OutputAsSVG(List<Vector3> lines)
+        {
+            StartCoroutine(CreatSVGDocument(lines));
+        }
+
+        private IEnumerator CreatSVGDocument(List<Vector3> lines)
+        {
+            StringBuilder svgStringBuilder = new StringBuilder();
+
+            var minX = lines.Min(vector => vector.x);
+            var maxX = lines.Max(vector => vector.x);
+
+            var minY = lines.Min(vector => vector.y);
+            var maxY = lines.Max(vector => vector.y);
+
+            var width = (maxX - minX);
+            var height = (maxY - minY);
+
+            svgStringBuilder.AppendLine($"<svg viewBox=\"0 0 {width} {height}\" xmlns=\"http://www.w3.org/2000/svg\">");
+            for (int i = 0; i < lines.Count; i += 2)
             {
-                if (outputProgress && i > 0) outputProgress.Invoke((float)i / lines.Count);
-                yield return new WaitForEndOfFrame();
+                if ((i % addLinesPerFrame) == 0)
+                {
+                    if (outputProgress && i > 0) outputProgress.Invoke((float)i / lines.Count);
+                    yield return new WaitForEndOfFrame();
+                }
+
+                var lineStart = lines[i];
+                var lineEnd = lines[i + 1];
+
+                lineStart.x = (lineStart.x - minX);
+                lineStart.y = (lineStart.y - minY);
+
+                lineEnd.x = (lineEnd.x - minX);
+                lineEnd.y = (lineEnd.y - minY);
+
+                svgStringBuilder.AppendLine($"<line x1=\"{lineStart.x}\" y1=\"{height - lineStart.y}\" x2=\"{lineEnd.x}\" y2=\"{height - lineEnd.y}\" stroke=\"black\" stroke-width=\"{strokeWidth}\" />");
             }
+            svgStringBuilder.AppendLine(" </svg>");
 
-            var lineStart = lines[i]; 
-            var lineEnd = lines[i+1];
+            if (outputProgress) outputProgress.Invoke(1.0f);
+            yield return new WaitForEndOfFrame();
+            SaveFile(svgStringBuilder);
 
-            lineStart.x = (lineStart.x-minX);
-            lineStart.y = (lineStart.y - minY);
-
-            lineEnd.x = (lineEnd.x - minX);
-            lineEnd.y = (lineEnd.y - minY);
-
-            svgStringBuilder.AppendLine($"<line x1=\"{lineStart.x}\" y1=\"{height-lineStart.y}\" x2=\"{lineEnd.x}\" y2=\"{height-lineEnd.y}\" stroke=\"black\" stroke-width=\"{strokeWidth}\" />");
         }
-        svgStringBuilder.AppendLine(" </svg>");
-
-        if (outputProgress) outputProgress.Invoke(1.0f);
-        yield return new WaitForEndOfFrame();
-        SaveFile(svgStringBuilder);
-
-    }
-    public void SaveFile(StringBuilder stringBuilder)
-    {
-#if UNITY_EDITOR 
-        string path = EditorUtility.SaveFilePanel("Save profile SVG", "", outputFileName, "svg");
-        if (path.Length != 0)
+        public void SaveFile(StringBuilder stringBuilder)
         {
-            Debug.Log($"Saving svg: {path}");
-            File.WriteAllText(path, stringBuilder.ToString());
-        }
+#if UNITY_EDITOR
+            string path = EditorUtility.SaveFilePanel("Save profile SVG", "", outputFileName, "svg");
+            if (path.Length != 0)
+            {
+                Debug.Log($"Saving svg: {path}");
+                File.WriteAllText(path, stringBuilder.ToString());
+            }
 #elif !UNITY_EDITOR && UNITY_WEBGL
         byte[] buffer = Encoding.ASCII.GetBytes(stringBuilder.ToString());
         DownloadFile(buffer, buffer.Length, outputFileName);
 #endif
+        }
     }
 }
