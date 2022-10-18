e6138b77780e67514cde98fc7d2749df5d838c66
Sam Baas <samuelpetrus@gmail.com>
2022-10-18 14:50:16 +0200

    detect vissim coordinates

--- Packages/Netherlands3D/Traffic/Runtime/Scripts/ConverterFZP.cs
+++ Packages/Netherlands3D/Traffic/Runtime/Scripts/ConverterFZP.cs
@@ -10,6 +10,13 @@ using Netherlands3D.Events;
 
 namespace Netherlands3D.Traffic
 {
+    public enum CoordinateInterpretation
+    {
+        WGS84,
+        RD,
+        UNITY,
+        AUTODETECT
+    }
     /// <summary>
     /// For converting vissim .fzp files
     /// </summary>
@@ -24,14 +31,18 @@ namespace Netherlands3D.Traffic
         /// </summary>
         private static readonly string requiredTemplate = "$VEHICLE:SIMSEC;NO;VEHTYPE;COORDFRONT;COORDREAR;WIDTH";
 
+        public static bool determineCoordinates = false;
+        public static CoordinateInterpretation coordinateInterpretation = CoordinateInterpretation.AUTODETECT;
         /// <summary>
         /// Reads the file.fzp with VISSIM data and converts it to useable vissim data
         /// </summary>
         /// <param name="filePath">The file to convert</param>
         /// <param name="maxDataCount">The max amount of data to be extracted. -1 = infinity</param>
         /// <param name="callback">The callback that gets triggerd when the data is collected.</param>
-        public static IEnumerator Convert(string filePath, int maxDataCount, Action<Dictionary<int, Data>> callback)
+        public static IEnumerator Convert(string filePath, int maxDataCount, Action<Dictionary<int, Data>> callback, CoordinateInterpretation useCoordinateInterpretation = CoordinateInterpretation.AUTODETECT)
         {
+            coordinateInterpretation = useCoordinateInterpretation;
+
             // Convert filePath to fileContent
             using StreamReader sr = new StreamReader(filePath);
             bool readyToConvert = false;
@@ -51,9 +62,12 @@ namespace Netherlands3D.Traffic
                     // Check if dataRaw id is already in convertedData
                     if(convertedData.ContainsKey(dataRaw.id))
                     {
+                        if(coordinateInterpretation == CoordinateInterpretation.AUTODETECT)
+                            DetectCoordinateInterpretation(dataRaw.coordinatesFront);
+
                         // Already exists, add new simulation second to its coordinates
                         // But check if coordinates arent already filled in if file contains data bugs/duplicates
-                        if(convertedData[dataRaw.id].coordinates.ContainsKey(dataRaw.simulationSecond))
+                        if (convertedData[dataRaw.id].coordinates.ContainsKey(dataRaw.simulationSecond))
                         {
                             Debug.LogWarning("[Traffic] Found a coordination duplicate. Make sure that each entity has its own unique simulation seconds.");
                             // This is caused by the entity id having 2 of the same simulation seconds in the data
@@ -61,15 +75,23 @@ namespace Netherlands3D.Traffic
                             continue;
                         }
 
+                        //Get coordinates converted based on unit setting or auto
+                        Vector3 coordinatesFront = ConvertToUnityCoordinate(dataRaw.coordinatesFront);
+                        Vector3 coordinatesRear = ConvertToUnityCoordinate(dataRaw.coordinatesRear);
+
                         // Add the new simulation second to its coordinates
-                        convertedData[dataRaw.id].coordinates.Add(dataRaw.simulationSecond, new Data.Coordinates(dataRaw.coordinatesFront, dataRaw.coordinatesRear));
+                        convertedData[dataRaw.id].coordinates.Add(dataRaw.simulationSecond, new Data.Coordinates(coordinatesFront, coordinatesRear));
                     }
                     else
                     {
+                        //Get coordinates converted based on unit setting or auto
+                        Vector3 coordinatesFront = ConvertToUnityCoordinate(dataRaw.coordinatesFront);
+                        Vector3 coordinatesRear = ConvertToUnityCoordinate(dataRaw.coordinatesRear);
+
                         // Doesnt exist, add it
                         convertedData.Add(dataRaw.id, new Data(dataRaw.id, dataRaw.vehicleTypeIndex, dataRaw.width,
                             new Dictionary<float, Data.Coordinates>()
-                            { { dataRaw.simulationSecond, new Data.Coordinates(dataRaw.coordinatesFront, dataRaw.coordinatesRear) } }));
+                            { { dataRaw.simulationSecond, new Data.Coordinates(coordinatesFront, coordinatesRear) } }));
                     }
 
                     //yield return null; // Wait a frame to not make the project freeze // Not needed since its fast now, maybe toggle it when handing very large data all at once?
@@ -117,6 +139,30 @@ namespace Netherlands3D.Traffic
             double z = double.Parse(splitString[2], CultureInfo.InvariantCulture);
 
             return new Vector3((float)x, (float)z, (float)y);
+        }
+
+        private static Vector3 ConvertToUnityCoordinate(Vector3 input)
+        {
+            return coordinateInterpretation switch
+            {
+                CoordinateInterpretation.WGS84 => CoordConvert.WGS84toUnity(input),
+                CoordinateInterpretation.RD => CoordConvert.RDtoUnity(input),
+                _ => input,
+            };
+        }
+
+        private static void DetectCoordinateInterpretation(Vector3 input)
+        {
+            if(CoordConvert.WGS84IsValid(new Vector3WGS(input.y, input.x, input.z)))
+            {
+                Debug.Log("Detected VISSIM WGS84 coordinates");
+                coordinateInterpretation = CoordinateInterpretation.WGS84;
+            }
+            else if(CoordConvert.RDIsValid(new Vector3RD(input.x, input.y, input.z)))
+            {
+                Debug.Log("Detected VISSIM RD coordinates");
+                coordinateInterpretation = CoordinateInterpretation.RD;
+            }
         }
     }
 }
