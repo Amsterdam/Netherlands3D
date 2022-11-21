0e8464a8f1640a76d79eee3b4a99b630fb56730c
Tom Simons <tom.v.simons@gmail.com>
2022-11-21 16:59:49 +0100

    added namespace

--- Packages/Netherlands3D/T3DPipeline/Runtime/Scripts/Annotations/ObjectClickHandler.cs
+++ Packages/Netherlands3D/T3DPipeline/Runtime/Scripts/Annotations/ObjectClickHandler.cs
@@ -4,19 +4,22 @@ using Netherlands3D.Events;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-/// <summary>
-/// Generic class to process a pointer click and fires the event with the 3D click position. Requires a PhysicsRaycaster to work
-/// </summary>
-[RequireComponent(typeof(Collider))]
-public class ObjectClickHandler : MonoBehaviour, IPointerClickHandler
+namespace Netherlands3D.T3DPipeline
 {
-    [Tooltip("Event that is called when the object is clicked")]
-    [SerializeField]
-    protected Vector3Event objectClicked;
-
-    public virtual void OnPointerClick(PointerEventData eventData)
+    /// <summary>
+    /// Generic class to process a pointer click and fires the event with the 3D click position. Requires a PhysicsRaycaster to work
+    /// </summary>
+    [RequireComponent(typeof(Collider))]
+    public class ObjectClickHandler : MonoBehaviour, IPointerClickHandler
     {
-        var pos = eventData.pointerCurrentRaycast.worldPosition;
-        objectClicked.Invoke(pos);
+        [Tooltip("Event that is called when the object is clicked")]
+        [SerializeField]
+        protected Vector3Event objectClicked;
+
+        public virtual void OnPointerClick(PointerEventData eventData)
+        {
+            var pos = eventData.pointerCurrentRaycast.worldPosition;
+            objectClicked.Invoke(pos);
+        }
     }
 }
