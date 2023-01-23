46a65626a6f3b6fb95c7ea6bd11f53aca9d1889d
Sam Baas <samuelpetrus@gmail.com>
2023-01-23 14:02:35 +0100

    public state for checking content loading/loaded state

--- Assets/3dTiles/tileset/Content.cs
+++ Assets/3dTiles/tileset/Content.cs
@@ -3,25 +3,44 @@ using System;
 using UnityEngine;
 
 [System.Serializable]
-public class Content: MonoBehaviour, IDisposable
+public class Content : MonoBehaviour, IDisposable
 {
     public string uri;
 
     public GameObject contentGameObject;
     private Coroutine runningContentRequest;
 
+    public enum ContentLoadState{
+        NOTLOADED,
+        LOADING,
+        READY,
+    }
+    public ContentLoadState state = ContentLoadState.NOTLOADED;
+
+    /// <summary>
+    /// Load the content from an url
+    /// </summary>
     public void Load()
     {
+        state = ContentLoadState.LOADING;
+
         runningContentRequest = StartCoroutine(ImportB3DMGltf.ImportBinFromURL(uri, GotContent));
     }
 
     private void GotContent(GameObject contentGameObject)
     {
+        state = ContentLoadState.READY;
+
         this.contentGameObject = contentGameObject;
     }
 
+    /// <summary>
+    /// Clean up coroutines and content gameobjects
+    /// </summary>
     public void Dispose()
     {
+        state = ContentLoadState.NOTLOADED;
+
         if (runningContentRequest != null)
             StopCoroutine(runningContentRequest);
 
