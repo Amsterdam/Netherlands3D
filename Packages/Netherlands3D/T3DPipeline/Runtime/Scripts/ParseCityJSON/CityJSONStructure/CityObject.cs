e05ca2431ad29488ec233da62904e18fdb419c21
Tom Simons <tom.v.simons@gmail.com>
2023-02-15 11:15:17 +0100

    made CityObject attributes public

--- Packages/Netherlands3D/T3DPipeline/Runtime/Scripts/ParseCityJSON/CityJSONStructure/CityObject.cs
+++ Packages/Netherlands3D/T3DPipeline/Runtime/Scripts/ParseCityJSON/CityJSONStructure/CityObject.cs
@@ -54,7 +54,7 @@ namespace Netherlands3D.T3DPipeline
         public Vector3Double AbsoluteCenter { get { return (MaxExtent + MinExtent) / 2; } }
 
         public List<CityGeometry> Geometries { get; protected set; }
-        protected List<CityObjectAttribute> attributes = new List<CityObjectAttribute>();
+        public List<CityObjectAttribute> Attributes { get; protected set; } = new List<CityObjectAttribute>();
 
         protected List<CityObject> cityChildren = new List<CityObject>();
         public CityObject[] CityChildren => cityChildren.ToArray();
@@ -196,7 +196,7 @@ namespace Netherlands3D.T3DPipeline
         protected virtual JSONObject GetAttributes()
         {
             var obj = new JSONObject();
-            foreach (var attribute in attributes)
+            foreach (var attribute in Attributes)
             {
                 obj.Add(attribute.Key, attribute.GetJSONValue());
             }
@@ -205,7 +205,7 @@ namespace Netherlands3D.T3DPipeline
 
         public void AddAttribute(CityObjectAttribute attribute)
         {
-            attributes.Add(attribute);
+            Attributes.Add(attribute);
         }
 
         public void FromJSONNode(string id, JSONNode cityObjectNode, CoordinateSystem coordinateSystem, List<Vector3Double> combinedVertices)
@@ -221,7 +221,7 @@ namespace Netherlands3D.T3DPipeline
                 Assert.IsTrue(CityGeometry.IsValidType(Type, geometry.Type));
                 Geometries.Add(geometry);
             }
-            attributes = CityObjectAttribute.ParseAttributesNode(this, cityObjectNode["attributes"]);
+            Attributes = CityObjectAttribute.ParseAttributesNode(this, cityObjectNode["attributes"]);
 
             var geographicalExtent = cityObjectNode["geographicalExtent"];
             if (geographicalExtent.Count > 0)
