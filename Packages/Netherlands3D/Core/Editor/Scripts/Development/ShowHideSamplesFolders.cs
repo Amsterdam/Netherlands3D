3eca5031e0e4f67983eca176a4c50228d6cfbb5f
Sam Baas <samuelpetrus@gmail.com>
2023-02-17 13:04:08 +0100

    moved assets and restored dependencies

--- Packages/Netherlands3D/Core/Editor/Scripts/Development/ShowHideSamplesFolders.cs
+++ Packages/Netherlands3D/Core/Editor/Scripts/Development/ShowHideSamplesFolders.cs
@@ -4,64 +4,92 @@ using UnityEngine;
 using UnityEditor;
 using System.IO;
 
-public class ShowHideSamplesFolders : MonoBehaviour
+namespace Netherlands3D.Development
 {
-    [MenuItem("Assets/Netherlands3D/Show samples folders")]
-    static void ShowSamples()
+    public class ShowHideSamplesFolders : MonoBehaviour
     {
-        Object selectedObject = Selection.activeObject;
-        if (selectedObject == null)
+        [MenuItem("Assets/Netherlands3D/Show Samples folders")]
+        static void ShowSamples()
         {
-            Debug.Log("Please select a folder");
-            return;
-        }
-
-        string assetPath = AssetDatabase.GetAssetPath(selectedObject);
-        string directory = Path.GetDirectoryName(assetPath);
-        if (Directory.Exists(directory))
-        {
-            RenameFolders(directory, "samples~", "samples");
-        }
-    }
-
-    [MenuItem("Assets/Netherlands3D/Hide samples folders")]
-    static void HideSamples()
-    {
-        Object selectedObject = Selection.activeObject;
-        if (selectedObject == null)
-        {
-            Debug.Log("Please select a folder");
-            return;
-        }
-
-        string assetPath = AssetDatabase.GetAssetPath(selectedObject);
-        string directory = Path.GetDirectoryName(assetPath);
-        if (Directory.Exists(directory))
-        {
-            RenameFolders(directory, "samples", "samples~");
-        }
-
-        var metaFile = directory + ".meta";
-        Debug.Log("Removing meta file if generated: " + metaFile);
-        if (File.Exists(directory + ".meta"))
-        {
-
-        }
-    }
-
-
-    static void RenameFolders(string folderPath, string from, string to)
-    {
-        foreach (string folder in Directory.GetDirectories(folderPath))
-        {
-            if (Path.GetFileName(folder) == from)
+            Object selectedObject = Selection.activeObject;
+            if (selectedObject == null)
             {
-                string newFolderPath = Path.Combine(Path.GetDirectoryName(folder), to);
-                Directory.Move(folder, newFolderPath);
+                Debug.Log("Please select a folder");
+                return;
+            }
+
+            string assetPath = AssetDatabase.GetAssetPath(selectedObject);
+            if (Directory.Exists(assetPath))
+            {
+                Debug.Log($"Recursively renaming 'Samples~' to 'Samples' folders in {assetPath}");
+                RenameFolders(assetPath, "Samples~", "Samples", true);
             }
             else
             {
-                RenameFolders(folder, from, to);
+                Debug.Log("Please select a folder");
+            }
+
+            AssetDatabase.Refresh();
+        }
+
+        [MenuItem("Assets/Netherlands3D/Hide Samples folders")]
+        static void HideSamples()
+        {
+            Object selectedObject = Selection.activeObject;
+            if (selectedObject == null)
+            {
+                Debug.Log("Please select a folder");
+                return;
+            }
+
+            string assetPath = AssetDatabase.GetAssetPath(selectedObject);
+            if (Directory.Exists(assetPath))
+            {
+                Debug.Log($"Recursively renaming 'Samples' to 'Samples~' folders in {assetPath}");
+                RenameFolders(assetPath, "Samples", "Samples~", true);
+            }
+            else
+            {
+                Debug.Log("Please select a folder");
+            }
+
+            
+
+            AssetDatabase.Refresh();
+        }
+
+        static void RenameFolders(string folderPath, string from, string to, bool removeMeta = false)
+        {
+            foreach (string folder in Directory.GetDirectories(folderPath))
+            {
+                if (Path.GetFileName(folder) == from)
+                {
+                    if (removeMeta) { 
+                        var metaFile = folder + ".meta";
+                        if (File.Exists(metaFile))
+                        {
+                            Debug.Log($"Removing meta file: {metaFile}");
+                            File.Delete(metaFile);
+                        }
+                    }
+                    string newFolderPath = Path.Combine(Path.GetDirectoryName(folder), to);
+                    var targetExists = Directory.Exists(newFolderPath);
+                    if (targetExists && Directory.GetFiles(newFolderPath).Length < 1)
+                    {
+                        Debug.Log($"Empty {newFolderPath} found. Removing.");
+                        Directory.Delete(newFolderPath);
+                    }
+                    else if (!targetExists)
+                    {
+                        Directory.Move(folder, newFolderPath);
+                        Debug.Log($"{folder} -> {newFolderPath}");
+                    }
+
+                }
+                else
+                {
+                    RenameFolders(folder, from, to, removeMeta);
+                }
             }
         }
     }
