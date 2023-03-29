608eb6e808ab2932a6fb6b40ab3914e5b7e33be4
Tom Simons <tom.v.simons@gmail.com>
2023-03-29 16:25:42 +0200

    allowed building colors to be set per tile, fixed waiting for meshes to be loaded and color data to be provided, order now does not matter

--- Packages/Netherlands3D/Core/Runtime/Scripts/TileSystem/ColorSubObjects.cs
+++ Packages/Netherlands3D/Core/Runtime/Scripts/TileSystem/ColorSubObjects.cs
@@ -4,6 +4,7 @@ using Netherlands3D.Events;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -12,9 +13,12 @@ namespace Netherlands3D.TileSystem
     public class ColorSubObjects : MonoBehaviour
     {
         private Dictionary<string, Color> idColors;
+        private Dictionary<Vector2Int, Dictionary<string, Color>> tileIdColors = new();
 
         [SerializeField]
         private bool disableOnStart = false;
+        [SerializeField]
+        private bool useManualIdColorInput;
 
         [Header("Listen to")]
         [SerializeField]
@@ -26,6 +30,8 @@ namespace Netherlands3D.TileSystem
         [SerializeField]
         private ObjectEvent onReceiveIdsAndFloatsForAlpha;
         [SerializeField]
+        private ObjectEvent onReceiveTileKeysIdsAndFloats;
+        [SerializeField]
         private GradientContainerEvent onSetGradient;
 
         [SerializeField]
@@ -35,7 +41,7 @@ namespace Netherlands3D.TileSystem
 
         [SerializeField]
         private TriggerEvent onClearData;
-    
+
         [Header("Sample float from gradient")]
         [SerializeField]
         private double minimumValue;
@@ -73,6 +79,18 @@ namespace Netherlands3D.TileSystem
                 this.enabled = true;
             }
 
+
+            if (onReceiveTileKeysIdsAndFloats)
+            {
+                onReceiveTileKeysIdsAndFloats.AddListenerStarted(SetTileKeysIDsAndFloatsAsColors);
+
+                //If we can receive ids+floats, add listeners to determine the min and max of the range
+                if (onReceiveMinRange) onReceiveMinRange.AddListenerStarted(SetMinRange);
+                if (onReceiveMaxRange) onReceiveMaxRange.AddListenerStarted(SetMaxRange);
+                if (onEnableDrawingColors) onEnableDrawingColors.InvokeStarted(true);
+                this.enabled = true;
+            }
+
             if (onReceiveIdsAndFloatsForAlpha)
             {
                 onReceiveIdsAndFloatsForAlpha.AddListenerStarted(SetIDsAndFloatsAsAlpha);
@@ -89,7 +107,7 @@ namespace Netherlands3D.TileSystem
                 onClearData.AddListenerStarted(ClearData);
             }
 
-            if(onSetGradient)
+            if (onSetGradient)
             {
                 onSetGradient.AddListenerStarted(SwapGradient);
             }
@@ -100,24 +118,24 @@ namespace Netherlands3D.TileSystem
             if (disableOnStart)
             {
                 this.enabled = false;
-                if(onEnableDrawingColors) onEnableDrawingColors.InvokeStarted(false);
+                if (onEnableDrawingColors) onEnableDrawingColors.InvokeStarted(false);
             }
         }
 
         public void SwapGradient(GradientContainer newGradientContainer)
-		{
+        {
             gradientContainer = newGradientContainer;
             UpdateColors(true);
         }
 
-		private void EnableDrawingColors(bool enable)
+        private void EnableDrawingColors(bool enable)
         {
             this.enabled = enable;
         }
 
         private void ClearData()
         {
-            if(idColors != null)
+            if (idColors != null)
                 idColors.Clear();
 
             ClearAllSubObjectColorData();
@@ -147,6 +165,34 @@ namespace Netherlands3D.TileSystem
             UpdateColors(true);
         }
 
+        public void SetTileKeysIDsAndFloatsAsColors(object tileKeysIdsAndColors)
+        {
+            var tileIdFloats = (Tuple<Vector2Int, Dictionary<string, float>>)tileKeysIdsAndColors;
+            var tileKey = tileIdFloats.Item1;
+            var idFloats = tileIdFloats.Item2;
+
+            //this.tileIdColors.Add(tileIdColors.Item1, tileIdColors.Item2);
+            //idColors = new Dictionary<string, Color>();
+
+            //foreach (var tileDictionary in this.tileIdColors)
+            //{
+            //    idColors.Union(tileDictionary.Value).ToDictionary(x => x.Key, x => x.Value);
+            //}
+            var tileIdColors = new Dictionary<string, Color>();
+            foreach (var keyValuePair in idFloats)
+            {
+                Color colorFromGradient = gradientContainer.gradient.Evaluate(Mathf.InverseLerp((float)minimumValue, (float)maximumValue, keyValuePair.Value));
+                tileIdColors.Add(keyValuePair.Key, colorFromGradient);
+            }
+            if (this.tileIdColors.ContainsKey(tileKey))
+                this.tileIdColors[tileKey] = tileIdColors;
+            else
+                this.tileIdColors.Add(tileKey, tileIdColors);
+
+            print("adding color dict for tile " + tileKey.ToString());
+            UpdateColorsByTileKey(tileIdFloats.Item1, tileIdColors);
+        }
+
         public void SetIDsAndFloatsAsColors(object idsAndFloats)
         {
             this.enabled = true;
@@ -169,7 +215,7 @@ namespace Netherlands3D.TileSystem
             foreach (var keyValuePair in idFloats)
             {
                 Color color = Color.white;
-                color.a = 1-Mathf.InverseLerp((float)minimumValue, (float)maximumValue, keyValuePair.Value);
+                color.a = 1 - Mathf.InverseLerp((float)minimumValue, (float)maximumValue, keyValuePair.Value);
                 idColors.Add(keyValuePair.Key, color);
             }
 
@@ -177,46 +223,78 @@ namespace Netherlands3D.TileSystem
         }
 
         private void OnDisable()
-		{
-			StopAllCoroutines();
-			ClearAllSubObjectColorData();
-		}
-
-		private void ClearAllSubObjectColorData()
-		{
-			var allSubObjects = gameObject.GetComponentsInChildren<SubObjects>();
-			for (int i = allSubObjects.Length - 1; i >= 0; i--)
-			{
-				allSubObjects[i].ResetColors();
-				Destroy(allSubObjects[i]);
-			}
-		}
-
-		private void OnTransformChildrenChanged()
         {
-            UpdateColors(false);
+            StopAllCoroutines();
+            ClearAllSubObjectColorData();
+        }
+
+        private void ClearAllSubObjectColorData()
+        {
+            var allSubObjects = gameObject.GetComponentsInChildren<SubObjects>();
+            for (int i = allSubObjects.Length - 1; i >= 0; i--)
+            {
+                allSubObjects[i].ResetColors();
+                Destroy(allSubObjects[i]);
+            }
+        }
+
+        private void OnTransformChildrenChanged()
+        {
+            if (useManualIdColorInput)
+                foreach (var tileKeyDictionaryPair in tileIdColors)
+                    UpdateColorsByTileKey(tileKeyDictionaryPair.Key, tileKeyDictionaryPair.Value);
+            else
+                UpdateColors(false);
         }
 
         private void UpdateColors(bool applyToExistingSubObjects = false)
         {
-            if (idColors == null) return;
+            if (useManualIdColorInput || idColors == null) return;
 
             foreach (Transform child in transform)
             {
+                Debug.Log(child.gameObject.name, child);
                 SubObjects subObjects = child.gameObject.GetComponent<SubObjects>();
                 if (!subObjects)
                 {
-                    if (child.gameObject.GetComponent<MeshFilter>())
-                    {
-                        subObjects = child.gameObject.AddComponent<SubObjects>();
-                        subObjects.ColorObjectsByID(idColors, defaultColor);
-                    }
+                    subObjects = child.gameObject.AddComponent<SubObjects>();
+                    subObjects.ColorObjectsByID(idColors, defaultColor);
                 }
                 else if (applyToExistingSubObjects)
                 {
                     subObjects.ColorObjectsByID(idColors, defaultColor);
                 }
             }
+        }
+
+        private void UpdateColorsByTileKey(Vector2Int tileKey, Dictionary<string, Color> idColorsOfTile)
+        {
+            if (!useManualIdColorInput)
+                Debug.LogWarning("Updating colors by tile key can result in unexpected behaviour if useManualIdColorInput is not enabled");
+
+            if (idColorsOfTile == null) return;
+
+            var subObjects = GetSubObjectsByTileKey(tileKey);
+            if (subObjects)
+                subObjects.ColorObjectsByID(idColorsOfTile, defaultColor);
+        }
+
+        public SubObjects GetSubObjectsByTileKey(Vector2Int tileKey)
+        {
+            print(transform.childCount);
+            foreach (Transform child in transform)
+            {
+                SubObjects subObjects = child.gameObject.GetComponent<SubObjects>();
+                if (!subObjects)
+                {
+                    subObjects = child.gameObject.AddComponent<SubObjects>();
+                }
+                if (subObjects.TileKey == tileKey)
+                {
+                    return subObjects;
+                }
+            }
+            return null;
         }
     }
 }
