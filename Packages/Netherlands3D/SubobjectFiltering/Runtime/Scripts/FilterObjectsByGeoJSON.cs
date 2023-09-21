818b58c3c5e636cdfb7b1062a2aad106bc100edc
Mike van Riel <mike@ingewikkeld.net>
2023-09-21 13:29:54 +0200

    Clean up import statements for SubjobjectFiltering
    Same as the previous commit, I wanted to remove the dependency on the
    core asmdef but in the FilterObjectByGeoJSON we depend on TileHandler
    code.

--- Packages/Netherlands3D/SubobjectFiltering/Runtime/Scripts/FilterObjectsByGeoJSON.cs
+++ Packages/Netherlands3D/SubobjectFiltering/Runtime/Scripts/FilterObjectsByGeoJSON.cs
@@ -1,25 +1,25 @@
 /*
-*  Copyright (C) X Gemeente
-*                X Amsterdam
-*                X Economic Services Departments
-*
-*  Licensed under the EUPL, Version 1.2 or later (the "License");
-*  You may not use this work except in compliance with the License.
-*  You may obtain a copy of the License at:
-*
-*    https://github.com/Amsterdam/Netherlands3D/blob/main/LICENSE.txt
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" basis,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-*  implied. See the License for the specific language governing
-*  permissions and limitations under the License.
-*/
-using Netherlands3D.Core;
-using Netherlands3D.Events;
+ *  Copyright (C) X Gemeente
+ *                X Amsterdam
+ *                X Economic Services Departments
+ *
+ *  Licensed under the EUPL, Version 1.2 or later (the "License");
+ *  You may not use this work except in compliance with the License.
+ *  You may obtain a copy of the License at:
+ *
+ *    https://github.com/Amsterdam/Netherlands3D/blob/main/LICENSE.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" basis,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using Netherlands3D.Coordinates;
+using Netherlands3D.Events;
 using Netherlands3D.GeoJSON;
 using UnityEngine;
 using UnityEngine.Networking;
